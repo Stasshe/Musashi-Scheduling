b04df6f3ad554cb19d98da57663a632f8009e593
Stasshe <egnm9stasshe@gmail.com>
2025-07-14 05:20:46 +0000

    bg-redだけが効かない謎バグ

--- types/index.ts
+++ types/index.ts
@@ -34,14 +34,3 @@ export interface UserProfile {
   name: string;
   registeredClasses: string[];
 }
-
-
-
-export const SUBJECT_COLORS = {
-  '数学': 'bg-blue-100 text-blue-800 border-blue-200',
-  '英語': 'bg-green-100 text-green-800 border-green-200',
-  '国語': 'bg-red-100 text-red-800 border-red-200',
-  '理科': 'bg-yellow-100 text-yellow-800 border-yellow-200',
-  '社会': 'bg-purple-100 text-purple-800 border-purple-200',
-  'その他': 'bg-gray-100 text-gray-800 border-gray-200'
-};
